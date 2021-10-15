7129cc0bede70f7038b8b0778baa1b0d9514268d
Przemyslaw Wiczolek <przemyslaw.wk@gmail.com>
2021-10-15 14:58:09 +0200

    Add more test cases

--- Battleships.Tests/BoardTests.cs
+++ Battleships.Tests/BoardTests.cs
@@ -9,29 +9,83 @@ namespace Battleships.Tests
 {
     public class BoardTests
     {
-        private readonly Random random = new();
+        private readonly Random random = new(123);
 
-        [Fact]
-        public void Init_ShouldGenerateAllShips()
+        [Theory]
+        [MemberData(nameof(BoardAndShipsTestData))]
+        public void Init_ShouldGenerateAllShips(
+            int boardHeight, int boardWidth,
+            int carriersNum,
+            int battleshipsNum,
+            int cruisersNum,
+            int submarinesNum,
+            int destroyersNum)
         {
             // Arrange
             var board = new Board(10, 10, random);
-            var ships = GetTestData_Init_ShouldGenerateAllShips();
+            var ships = GenerateShips(carriersNum, battleshipsNum, cruisersNum, submarinesNum, destroyersNum);
 
             // Act
             board.Init(ships);
 
             // Assert
+            // Area sum matches
             var shipsAreaSum = ships.Select(x => x.Size).Sum();
             Assert.Equal(shipsAreaSum, board.UpFields);
+
+            // Up-fields set correctly
+            int upFieldsCount = 0;
+            for (int i = 0; i < board.Height; ++i)
+            {
+                for (int j = 0; j < board.Width; ++j)
+                {
+                    if (board[i, j] == Field.ShipUp)
+                        ++upFieldsCount;
+                }
+            }
+            Assert.Equal(upFieldsCount, board.UpFields);
         }
 
-        private List<Ship> GetTestData_Init_ShouldGenerateAllShips(
-            int carriersNum = 5,
-            int battleshipsNum = 4,
-            int cruisersNum = 3,
-            int submarinesNum = 3,
-            int destroyersNum = 2)
+        public static IEnumerable<object[]> BoardAndShipsTestData =>
+        new List<object[]>
+        {
+            new object[] { 10, 10,  // even size
+                5, 4, 3, 3, 2 },
+            new object[] { 9, 9,    // odd size
+                5, 4, 3, 3, 2 },
+            new object[] { 25, 5,   // very high
+                5, 4, 3, 3, 2 },
+            new object[] { 5, 24,   // very wide
+                5, 4, 3, 3, 2 },
+            new object[]{ 10, 10,  // only destroyers
+                0, 0, 0, 0, 19},
+            new object[]{ 20, 1,   // only carriers + full board
+                20, 0, 0, 0, 0},
+            new object[]{ 3, 3,    // small board
+                1, 1, 1, 0, 0},
+            new object[]{ 9, 9,    // empty
+                0, 0, 0, 0, 0}
+        };
+
+
+        [Fact]
+        public void Init_WhenShipsSizeExceedBoardArea_ShouldThrow()
+        {
+
+        }
+
+        [Fact]
+        public void Init_WhenShipSizeExceedsDimensionOfBoard_ShouldThrow()
+        {
+
+        }
+
+        private List<Ship> GenerateShips(
+           int carriersNum = 5,
+           int battleshipsNum = 4,
+           int cruisersNum = 3,
+           int submarinesNum = 3,
+           int destroyersNum = 2)
         {
             var ships = new List<Ship>();
 
@@ -51,19 +105,6 @@ namespace Battleships.Tests
                 ships.Add(new Destroyer());
 
             return ships;
-        }
-
-
-        [Fact]
-        public void Init_WhenShipsSizeExceedBoardArea_ShouldThrow()
-        {
-
-        }
-
-        [Fact]
-        public void Init_WhenShipSizeExceedsDimensionOfBoard_ShouldThrow()
-        {
-
         }
     }
 }
